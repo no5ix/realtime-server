2523a8510bcb1ff35b7acb41aa370e3df4dcd93b
no5ix <1145263208@qq.com>
2018-05-04 15:24:56 +0800

    add screenShot & readme & window mode

---
+++ README.md
@@ -0,0 +1,22 @@
+a real time game server and a UE4 demo for it. 
+
+一个实时游戏服务器和一个对应的UE4游戏前端demo.
+
+
+![UE4DemoScreenshot.png](./img/UE4DemoScreenshot.png)
+
+
+# Download & Play
+
+ 
+- Client Side : [UE4ClientDemo.exe (Win32)](https://pan.baidu.com/s/1nopKSBXH1bqrE-c5DaSyxw)
+
+- Server Side : a server instance is running on my VPS, so just double click the UE4ClientDemo.exe that will connect to my server automatically
+
+
+# About This 
+
+
+- Unreal Engine Version : 4.19
+
+Enjoy!
---
+++ UE4ClientDemo/Source/UE4ClientDemo/RealTimeSrv/PutMeInTheMap.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PutMeInTheMap.h"
+
+
+// Sets default values
+APutMeInTheMap::APutMeInTheMap()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APutMeInTheMap::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APutMeInTheMap::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
