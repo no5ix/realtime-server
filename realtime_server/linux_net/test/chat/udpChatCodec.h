b6688268b50476cff623dbd865d9ff7aceb73b03
no5ix <1145263208@qq.com>
2018-06-21 15:46:06 +0800

    update .gitignore

---
+++ realtime_server/linux_net/test/chat/udpChatCodec.h
@@ -0,0 +1,67 @@
+#ifndef MUDUO_EXAMPLES_ASIO_CHAT_CODEC_H
+#define MUDUO_EXAMPLES_ASIO_CHAT_CODEC_H
+
+#include <muduo/base/Logging.h>
+#include <muduo/net/Buffer.h>
+#include <muduo/net/Endian.h>
+#include <net/UdpConnection.h>
+
+class UdpLengthHeaderCodec : muduo::noncopyable
+{
+public:
+	typedef std::function<void( const muduo::net::UdpConnectionPtr&,
+		const muduo::string& message,
+		muduo::Timestamp )> StringMessageCallback;
+
+	explicit UdpLengthHeaderCodec( const StringMessageCallback& cb )
+		: messageCallback_( cb )
+	{}
+
+	void onMessage( const muduo::net::UdpConnectionPtr& conn,
+		muduo::net::Buffer* buf,
+		muduo::Timestamp receiveTime )
+	{
+		while ( buf->readableBytes() >= kHeaderLen ) // kHeaderLen == 4
+		{
+			// FIXME: use Buffer::peekInt32()
+			const void* data = buf->peek();
+			int32_t be32 = *static_cast< const int32_t* >( data ); // SIGBUS
+			const int32_t len = muduo::net::sockets::networkToHost32( be32 );
+			if ( len > 65536 || len < 0 )
+			{
+				LOG_ERROR << "Invalid length " << len;
+				conn->shutdown();  // FIXME: disable reading
+				break;
+			}
+			else if ( buf->readableBytes() >= len + kHeaderLen )
+			{
+				buf->retrieve( kHeaderLen );
+				muduo::string message( buf->peek(), len );
+				messageCallback_( conn, message, receiveTime );
+				buf->retrieve( len );
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+
+	// FIXME: UdpConnectionPtr
+	void send( muduo::net::UdpConnection* conn,
+		const muduo::StringPiece& message )
+	{
+		muduo::net::Buffer buf;
+		buf.append( message.data(), message.size() );
+		int32_t len = static_cast< int32_t >( message.size() );
+		int32_t be32 = muduo::net::sockets::hostToNetwork32( len );
+		buf.prepend( &be32, sizeof be32 );
+		conn->send( &buf );
+	}
+
+private:
+	StringMessageCallback messageCallback_;
+	const static size_t kHeaderLen = sizeof( int32_t );
+};
+
+#endif  // MUDUO_EXAMPLES_ASIO_CHAT_CODEC_H
---
+++ realtime_server/linux_net/test/chat/udpChatSrv_highperformance.cpp
@@ -0,0 +1,129 @@
+#include "udpChatCodec.h"
+
+#include <muduo/base/Logging.h>
+#include <muduo/base/Mutex.h>
+#include <muduo/base/ThreadLocalSingleton.h>
+#include <muduo/net/EventLoop.h>
+#include <net/UdpServer.h>
+
+
+#include <set>
+#include <stdio.h>
+#include <unistd.h>
+
+using namespace muduo;
+using namespace muduo::net;
+
+class ChatServer : noncopyable
+{
+public:
+	ChatServer( EventLoop* loop,
+		const InetAddress& listenAddr )
+		: server_( loop, listenAddr, "ChatServer" ),
+		codec_( std::bind( &ChatServer::onStringMessage, this, _1, _2, _3 ) )
+	{
+		server_.setConnectionCallback(
+			std::bind( &ChatServer::onConnection, this, _1 ) );
+		server_.setMessageCallback(
+			std::bind( &UdpLengthHeaderCodec::onMessage, &codec_, _1, _2, _3 ) );
+	}
+
+	void setThreadNum( int numThreads )
+	{
+		server_.setThreadNum( numThreads );
+	}
+
+	void start()
+	{
+		server_.setThreadInitCallback( std::bind( &ChatServer::threadInit, this, _1 ) );
+		server_.start();
+	}
+
+private:
+	void onConnection( const UdpConnectionPtr& conn )
+	{
+		LOG_INFO << conn->localAddress().toIpPort() << " -> "
+			<< conn->peerAddress().toIpPort() << " is "
+			<< ( conn->connected() ? "UP" : "DOWN" );
+
+		if ( conn->connected() )
+		{
+			LocalConnections::instance().insert( conn );
+		}
+		else
+		{
+			LocalConnections::instance().erase( conn );
+		}
+	}
+
+	void onStringMessage( const UdpConnectionPtr&,
+		const string& message,
+		Timestamp )
+	{
+		EventLoop::Functor f = std::bind( &ChatServer::distributeMessage, this, message );
+		LOG_DEBUG;
+
+		MutexLockGuard lock( mutex_ );
+		for ( std::set<EventLoop*>::iterator it = loops_.begin();
+			it != loops_.end();
+			++it )
+		{
+			( *it )->queueInLoop( f );
+		}
+		LOG_DEBUG;
+	}
+
+	typedef std::set<UdpConnectionPtr> ConnectionList;
+
+	void distributeMessage( const string& message )
+	{
+		LOG_DEBUG << "begin";
+		for ( ConnectionList::iterator it = LocalConnections::instance().begin();
+			it != LocalConnections::instance().end();
+			++it )
+		{
+			codec_.send( get_pointer( *it ), message );
+		}
+		LOG_DEBUG << "end";
+	}
+
+	void threadInit( EventLoop* loop )
+	{
+		assert( LocalConnections::pointer() == NULL );
+		LocalConnections::instance();
+		assert( LocalConnections::pointer() != NULL );
+		MutexLockGuard lock( mutex_ );
+		loops_.insert( loop );
+	}
+
+	UdpServer server_;
+	UdpLengthHeaderCodec codec_;
+	typedef ThreadLocalSingleton<ConnectionList> LocalConnections;
+
+	MutexLock mutex_;
+	std::set<EventLoop*> loops_;
+};
+
+int main( int argc, char* argv[] )
+{
+	LOG_INFO << "pid = " << getpid();
+	if ( argc > 1 )
+	{
+		EventLoop loop;
+		uint16_t port = static_cast< uint16_t >( atoi( argv[1] ) );
+		InetAddress serverAddr( port );
+		ChatServer server( &loop, serverAddr );
+		if ( argc > 2 )
+		{
+			server.setThreadNum( atoi( argv[2] ) );
+		}
+		server.start();
+		loop.loop();
+	}
+	else
+	{
+		printf( "Usage: %s port [thread_num]\n", argv[0] );
+	}
+}
+
+
