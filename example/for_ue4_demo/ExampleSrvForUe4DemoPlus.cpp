5600da2f1e79f0d78265d121249aced84ca7d49b
no5ix <1145263208@qq.com>
2018-07-17 23:44:42 +0800

    add comment

--- example/for_ue4_demo/ExampleSrvForUe4DemoPlus.cpp
+++ example/for_ue4_demo/ExampleSrvForUe4DemoPlus.cpp
@@ -40,13 +40,6 @@ public:
 		return  std::static_pointer_cast< GameObj >( newCharacter );
 	}
 
-	//	SimulateRealWorldOnWin() 功能为 :
-	//	在Windows上 ( Linux为了减少代码分支, 不提供此功能 ), 
-	//	如果输入命令:   ./example_for_ue4_demo.exe 0.3 0.8 1
-	//	则服务器将会模拟 : 
-	//	-	模拟延迟为 0.3 : 当前延迟+300毫秒的延迟, 若当前延迟为30ms, 则模拟之后的延迟约为30+300=330左右
-	//	-	模拟丢包率为 0.8 : 百分之八十的丢包率
-	//	-	模拟网络抖动为 1 : 1为随机抖动, 0为不抖动
 	void Run() { server_.SimulateRealWorldOnWin(); server_.Run(); }
 
 private:
